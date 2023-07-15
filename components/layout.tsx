efaa3529ca4fd50fee920400c8d492dd17ed6576
Abhisek Mazumdar <abhisekmazumdar@users.noreply.github.com>
2023-07-16 01:47:23 +0530

    Header & articles listing page fixes. (#2)

--- components/layout.tsx
+++ components/layout.tsx
@@ -9,20 +9,23 @@ export function Layout({ children }) {
       <div className="max-w-screen-md px-6 mx-auto">
         <header>
           <div className="container flex items-center justify-between py-6 mx-auto">
-            <Link href="/" className="text-2xl font-semibold no-underline">
+            <Link href="/" className="text-2xl font-semibold no-underline hidden sm:block">
               abhisek.xyz
             </Link>
-            <Link href="/thoughts" className="hover:text-blue-600">
+            <Link href="/" className="text-2xl font-semibold no-underline md:hidden">
+              Hi!
+            </Link>
+            <Link href="/thoughts" className="hover:text-blue-600 underline">
               my thoughts
             </Link>
-            <Link href="/use" className="hover:text-blue-600">
+            <Link href="/use" className="hover:text-blue-600 underline">
               what i use
             </Link>
             <Link
               href="https://abhisekmazumdar.notion.site/Abhisek-Mazumdar-f624928ce0fa4b48900097a7c6703de9"
               target="_blank"
               rel="external"
-              className="hover:text-blue-600"
+              className="hover:text-blue-600 underline"
             >
               know me more!
             </Link>
